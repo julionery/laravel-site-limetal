a59d053df48a3c9a4790a3e36c100a5b6880aaa2
Julio Cesar Almeida Nery <julio@bonzay.net>
2017-03-29 10:42:58 -0300

    Config Database

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'us-cdbr-iron-east-03.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_d5454d5f736b747'),
+            'username' => env('DB_USERNAME', 'b56e02baadf88b'),
+            'password' => env('DB_PASSWORD', 'd61e6bcb'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
